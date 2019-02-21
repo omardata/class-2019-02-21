e8505f233f842b73822a71e0aeb211e8579a41f7
Omar Cano <ocano9504@gmail.com>
2019-02-21 14:20:25 -0500

    Second

---
+++ Firstgo.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(readxl)
+
+elec <- read_csv(file = "https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-az06-3.csv",
+                 col_types =cols(
+                   .default = col_character(),
+                   turnout_scale = col_double(),
+                   turnout_score = col_double(),
+                   w_LV = col_double(),
+                   w_RV = col_double(),
+                   final_weight = col_double(),
+                   timestamp = col_datetime(format = "")
+                 )) 
+   
+
+
+glimpse(elec)
+
+elec %>%   
+  select(response, educ4, final_weight ) %>% 
+  group_by(response, educ4) %>% 
+  filter(! educ4 =="[DO NOT READ] Don't Knoq/Refused") %>% 
+  summarize(total= n()) %>% 
+  spread(key= response, value=  total)
+  
+  
+  
+            
+
+View(elec) 
+
